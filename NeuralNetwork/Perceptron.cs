26660d09cd9f8d10dd67b29ad12b3752323b336e
Yuri <markus@matrix-net.de>
2023-02-13 19:53:09 +0100

    Projektdateien hinzufügen.

---
+++ NeuralNetwork/Perceptron.cs
@@ -0,0 +1,30 @@
+﻿namespace NeuralNetwork
+{
+    internal class Perceptron
+    {
+        public Perceptron(int numberOfInputs)
+        {
+            Inputs = new double[numberOfInputs];
+            Weights = new double[numberOfInputs];
+        }
+
+        public double[] Inputs { get; }
+        public double[] Weights { get; }
+        public double Bias { get; set; }
+
+        public Func<double, double> ActivationFunction { get; set; }
+
+        public double Output
+        {
+            get
+            {
+                var s = Bias;
+                for (var i = 0; i < Inputs.Length; i++)
+                {
+                    s += Inputs[i] * Weights[i];
+                }
+                return ActivationFunction(s);
+            }
+        }
+    }
+}
---
+++ NeuralNetwork/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeuralNetwork;
+
+Console.WriteLine("Hello, World!");
+
+Test.TestLogicalOr();
+
+static class Test
+{
+    [TestMethod]
+    public static void TestLogicalOr()
+    {
+        var perceptron = new Perceptron(2);
+        perceptron.ActivationFunction = x => x >= 1.0 ? 1.0 : 0.0;
+        perceptron.Bias = 0;
+        perceptron.Weights[0] = 1;
+        perceptron.Weights[1] = 1;
+
+        perceptron.Inputs[0] = 0;
+        perceptron.Inputs[1] = 0;
+        Assert.AreEqual(0, perceptron.Output);
+        Console.WriteLine(perceptron.Output);
+
+        perceptron.Inputs[0] = 0;
+        perceptron.Inputs[1] = 1;
+        Assert.AreEqual(1, perceptron.Output);
+        Console.WriteLine(perceptron.Output);
+
+        perceptron.Inputs[0] = 1;
+        perceptron.Inputs[1] = 0;
+        Assert.AreEqual(1, perceptron.Output);
+        Console.WriteLine(perceptron.Output);
+
+        perceptron.Inputs[0] = 1;
+        perceptron.Inputs[1] = 1;
+        Assert.AreEqual(1, perceptron.Output);
+        Console.WriteLine(perceptron.Output);
+    }
+}
